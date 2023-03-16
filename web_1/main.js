8d2338167803498d5bc9a0032a54e053be637195
nihal4 <sm.nihal4@gmail.com>
2023-03-16 16:55:45 +0600

    done_40

--- web_1/main.js
+++ web_1/main.js
@@ -1,2 +1,10 @@
-name = "nihal";
-document.write(name)
+var popo = document.querySelector('#head_id');
+var line1 = document.querySelector('.burgerline1');
+var line2 = document.querySelector('.burgerline2');
+var line3 = document.querySelector('.burgerline3');
+function po(){
+        popo.classList.toggle("head_id_1")
+        line1.classList.toggle('bug1')
+        line2.classList.toggle('bug2')
+        line3.classList.toggle('bug3')
+}
