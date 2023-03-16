6b058a5f14147a8b62add7f74d4dac745a6be992
nihal4 <sm.nihal4@gmail.com>
2023-03-16 20:58:34 +0600

    oki

--- web_2/js/main.js
+++ web_2/js/main.js
@@ -1,9 +1,10 @@
 var burger_line1 = document.querySelector('.navbar-burger-line1');
 var burger_line2 = document.querySelector('.navbar-burger-line2');
 var burger_line3 = document.querySelector('.navbar-burger-line3');
-
+var navbar_list = document.querySelector('.navbar-list')
 function make_cross(){
     burger_line1.classList.toggle('line1')
     burger_line2.classList.toggle('line2')
     burger_line3.classList.toggle('line3')
+    navbar_list.classList.toggle('toggle')
 }
