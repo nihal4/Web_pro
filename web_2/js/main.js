101ca88acdda8e2498c294311266c86079c5f775
nihal4 <sm.nihal4@gmail.com>
2023-03-17 14:43:00 +0600

    kk

--- web_2/js/main.js
+++ web_2/js/main.js
@@ -22,3 +22,21 @@ window.onscroll = function(){
     }
 }
 
+//loading screen 
+function onReady(callback) {
+    var intervalId = window.setInterval(function() {
+      if (document.getElementsByTagName('body')[0] !== undefined) {
+        window.clearInterval(intervalId);
+        callback.call(this);
+      }
+    }, 1000);
+  }
+  
+  function setVisible(selector, visible) {
+    document.querySelector(selector).style.display = visible ? 'block' : 'none';
+  }
+  
+  onReady(function() {
+    setVisible('.header', true);
+    setVisible('#loading', false);
+  });
