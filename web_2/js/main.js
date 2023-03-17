ceb09c59d5c85fee385429ff20f5bb938fa2d387
nihal4 <sm.nihal4@gmail.com>
2023-03-17 17:48:19 +0600

    ok

--- web_2/js/main.js
+++ web_2/js/main.js
@@ -2,14 +2,19 @@ var burger_line1 = document.querySelector('.navbar-burger-line1');
 var burger_line2 = document.querySelector('.navbar-burger-line2');
 var burger_line3 = document.querySelector('.navbar-burger-line3');
 var navbar_list = document.querySelector('.navbar-list')
+var navbar_part2 = document.querySelector('.navbar-part2')
+var navabr = document.querySelector('.navbar');
 function make_cross(){
+    navabr.classList.toggle('add_100')
+    navbar_part2.classList.toggle('add_dis')
     burger_line1.classList.toggle('line1')
     burger_line2.classList.toggle('line2')
     burger_line3.classList.toggle('line3')
     navbar_list.classList.toggle('toggle')
+
 }
 
-var navabr = document.querySelector('.navbar');
+
 
 window.onscroll = function(){
     if(document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200){
@@ -40,3 +45,4 @@ function onReady(callback) {
     setVisible('.header', true);
     setVisible('#loading', false);
   });
+
