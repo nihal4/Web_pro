5c25ceca1e1c4da38f44de5361a3250bfece91de
Nihal <62196755+nihal4@users.noreply.github.com>
2023-03-22 16:25:23 +0600

    ok

--- web_3/js/main.js
+++ web_3/js/main.js
@@ -1,6 +1,5 @@
 const light = document.querySelector(".light");
-const menu = document.querySelectorAll('#lines')
-console.log(menu)
+const menu = document.querySelectorAll("#lines");
 //
 // function to set a given theme/color-scheme
 function setTheme(themeName) {
@@ -12,20 +11,17 @@ function toggleTheme() {
   if (localStorage.getItem("theme") === "theme-dark") {
     setTheme("theme-light");
     light.classList.remove("white");
-    menu[0].classList.remove('white');
-    menu[1].classList.remove('white');
-    menu[2].classList.remove('white');
-    document.querySelector('.link').classList.remove('white');
-
-    
+    menu[0].classList.remove("white");
+    menu[1].classList.remove("white");
+    menu[2].classList.remove("white");
+    document.querySelector(".link").classList.remove("white");
   } else {
     setTheme("theme-dark");
-    document.querySelector('.link').classList.add('white');
+    document.querySelector(".link").classList.add("white");
     light.classList.add("white");
-    menu[0].classList.add('white');
-    menu[1].classList.add('white');
-    menu[2].classList.add('white');
-    
+    menu[0].classList.add("white");
+    menu[1].classList.add("white");
+    menu[2].classList.add("white");
   }
 }
 // Immediately invoked function to set the theme on initial load
@@ -33,19 +29,17 @@ function toggleTheme() {
   if (localStorage.getItem("theme") === "theme-dark") {
     setTheme("theme-dark");
     light.classList.add("white");
-    document.querySelector('.link').classList.add('white');
-    menu[0].classList.add('white');
-    menu[1].classList.add('white');
-    menu[2].classList.add('white');
-    
+    document.querySelector(".link").classList.add("white");
+    menu[0].classList.add("white");
+    menu[1].classList.add("white");
+    menu[2].classList.add("white");
   } else {
     setTheme("theme-light");
-    document.querySelector('.link').classList.remove('white');
+    document.querySelector(".link").classList.remove("white");
     light.classList.remove("white");
-    menu[0].classList.remove('white');
-    menu[1].classList.remove('white');
-    menu[2].classList.remove('white');
-    
+    menu[0].classList.remove("white");
+    menu[1].classList.remove("white");
+    menu[2].classList.remove("white");
   }
 })();
 
@@ -54,7 +48,10 @@ function toggleTheme() {
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Hi my name is Nihal..","I am a passionate front-end web developer"];
+const textArray = [
+  "Hi my name is Nihal..",
+  "I am a passionate front-end web developer",
+];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000; // Delay between current and next text
@@ -98,81 +95,113 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 //new fade in & fade away
-function fade(fade_name,fade_name2){
-  localStorage.setItem('fade',fade_name)
-  localStorage.setItem('fade2',fade_name2)
-  document.querySelector('.hero').classList.add(fade_name);
-  document.querySelector('.s1').classList.add(fade_name2)
+function fade(fade_name, fade_name2) {
+  localStorage.setItem("fade", fade_name);
+  localStorage.setItem("fade2", fade_name2);
+  document.querySelector(".hero").classList.add(fade_name);
+  document.querySelector(".s1").classList.add(fade_name2);
 }
 
-function example(){
-  if(localStorage.getItem("fade") === 'fade_away' && localStorage.getItem('fade2')=='fade_in'){
-      fade('fade_in','fade_away');
-      document.querySelector('.hero').classList.remove('fade_away');
-  }else{
-    fade('fade_away','fade_in')
-    
-    document.querySelector('.s1').classList.remove('fade_away');
+function example() {
+  if (
+    localStorage.getItem("fade") === "fade_away" &&
+    localStorage.getItem("fade2") == "fade_in"
+  ) {
+    fade("fade_in", "fade_away");
+    document.querySelector(".hero").classList.remove("fade_away");
+  } else {
+    fade("fade_away", "fade_in");
+
+    document.querySelector(".s1").classList.remove("fade_away");
   }
 }
-(function(){
-  if(localStorage.getItem('fade')==='fade_away' && localStorage.getItem('fade2')=='fade_in'){
-    fade('fade_away','fade_in')
-  }else{
-    fade('fade_in','fade_away')
+(function () {
+  if (
+    localStorage.getItem("fade") === "fade_away" &&
+    localStorage.getItem("fade2") == "fade_in"
+  ) {
+    fade("fade_away", "fade_in");
+  } else {
+    fade("fade_in", "fade_away");
   }
 })();
 
 //noclick
 
-var box2 = document.querySelector('.profile-box2');
-var box1 = document.querySelector('.profile-box1');
-function show(){
-  menu[0].classList.toggle('rotate1')
-  menu[1].classList.toggle('rotate3')
-  menu[2].classList.toggle('rotate2')
+var box2 = document.querySelector(".profile-box2");
+var box1 = document.querySelector(".profile-box1");
+function show() {
+  menu[0].classList.toggle("rotate1");
+  menu[1].classList.toggle("rotate3");
+  menu[2].classList.toggle("rotate2");
 }
 //
-var proName = document.querySelector('.profile-name');
+var proName = document.querySelector(".profile-name");
 
-names = ['Sir potato the third','potato boiii','nihal ahmed']
+names = ["Sir potato the third", "potato boiii", "nihal ahmed"];
 
-textSequense(0)
-function textSequense(i){
-  if(names.length >i){
-    setTimeout(function(){
-      proName.innerHTML = names[i]
-      textSequense(++i)
-    },3000)
-  }else if (names.length==i){
-    textSequense(0)
+textSequense(0);
+function textSequense(i) {
+  if (names.length > i) {
+    setTimeout(function () {
+      proName.innerHTML = names[i];
+      textSequense(++i);
+    }, 3000);
+  } else if (names.length == i) {
+    textSequense(0);
   }
 }
 
 //function float()
-var link = document.querySelector('.link');
-var logoIcon = document.querySelector('.devicon-github-original')
-var links = ['https://www.facebook.com/vrubivrom/','https://www.google.com/','https://github.com/nihal4']
-var logos = ['fab fa-facebook-f','fab fa-instagram','devicon-github-original']
-logo(0)
-function logo(i){
-  if(links.length >i){
-    setTimeout(function(){
-      link.href = links[i]
-      logoIcon.className = logos[i]
-      logo(++i)
-    },5000)
-  }else if (links.length==i){
-    logo(0)
+var link = document.querySelector(".link");
+var logoIcon = document.querySelector(".devicon-github-original");
+var links = [
+  "https://www.facebook.com/vrubivrom/",
+  "https://www.google.com/",
+  "https://github.com/nihal4",
+];
+var logos = [
+  "fab fa-facebook-f",
+  "fab fa-instagram",
+  "devicon-github-original",
+];
+logo(0);
+function logo(i) {
+  if (links.length > i) {
+    setTimeout(function () {
+      link.href = links[i];
+      logoIcon.className = logos[i];
+      logo(++i);
+    }, 5000);
+  } else if (links.length == i) {
+    logo(0);
   }
 }
 
 //new slide apper
-
-var allBox = document.querySelectorAll('.profile-skills-box')
-
-function slide_left(i){
-  if(allBox.length>i){
-    
+var child = document.querySelector('.profile-skills-box');
+allNodes = child.childNodes;
+console.log(allNodes)
+var arr1 = ['devicon-javascript-plain','devicon-python-plain-wordmark','devicon-cplusplus-plain']
+var arr2 = ['devicon-sass-original','devicon-c-plain','devicon-blender-original']
+slider(0)
+function slider(i){
+  if(arr1.length>i && arr2.length>i){
+    setTheme(function(){
+      allNodes[1].className =arr1[i]
+      allNodes[3].className =arr2[i]
+      slider(++i)
+    },300)
+  }else if(arr1.length == i && arr2.length == i){
+    slider(0)
   }
 }
+
+
+
+//devicon-javascript-plain
+//devicon-sass-original
+//devicon-python-plain-wordmark
+//devicon-c-plain
+//devicon-cplusplus-plain
+//devicon-blender-original
